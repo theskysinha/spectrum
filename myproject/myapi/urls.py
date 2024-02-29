5044f953f55c611049776a6c540ffbc51672758a
Akash Sinha <66449020+theskysinha@users.noreply.github.com>
2024-02-29 15:44:31 +0530

    updated login form

--- myproject/myapi/urls.py
+++ myproject/myapi/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    path('hello-world/', views.hello_world, name='hello_world'),
+    re_path('login', views.login),
+    re_path('signup', views.signup),
+    re_path('test-token', views.test_token),
 ]
--- myproject/myapi/views.py
+++ myproject/myapi/views.py
@@ -2,6 +2,38 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from .serializers import UserSerializer
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+@api_view(['POST'])
+def login(request):
+    user = get_object_or_404(User, username=request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance = user)
+    return Response({'token': token.key, 'user': serializer.data}, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+def signup(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        user.set_password(user.password)
+        user.save()
+        token = Token.objects.create(user=user)
+        return Response({'token': token.key, 'user': serializer.data}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 @api_view(['GET'])
-def hello_world(request):
-    return Response({'message': 'Hello, world!'})
+@authentication_classes([TokenAuthentication, SessionAuthentication])
+@permission_classes([IsAuthenticated])
+def test_token(request):
+    return Response({'message': '{} are authenticated'.format(request.user.username)}, status=status.HTTP_200_OK)
